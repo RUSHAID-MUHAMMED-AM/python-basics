e9c9f879aa7cd6e63f8e63c7a723ba3b4ed85315
RUSHAID-MUHAMMED-AM <rushaidmuhammedam@gmail.com>
2025-07-26 16:00:26 +0530

    fifth

--- loop.py
+++ loop.py
@@ -1,11 +1,11 @@
-# #1Print the even numbers from 2 to 20 using a for loop.
+# #1 Print the even numbers from 2 to 20 using a for loop.
 
 # for i in range(2,21):
 #     if i%2==0:
 #         print(i)
 
 
-# #2Print the sum of all numbers from 1 to 50 using a for loop
+# #2 Print the sum of all numbers from 1 to 50 using a for loop
 
 # k=0;
 # for i in range(1,51):
@@ -14,9 +14,18 @@
     
 
 
-#Print the sum of all numbers from 1 to 50 using a for loop
+# #3 Print the sum of all numbers from 1 to 50 using a for loop
 
-k=0;
-for i in range(1,51):
-    k+=i;
-print(k);
+# k=0;
+# for i in range(1,51):
+#     k+=i;
+# print(k);
+
+
+
+#4 Print the multiplication table of 7 using a for loop (from 1 to 10).
+
+
+for i in range(1,11):
+    k=7*i
+    print("7 *",i,"=",k)
