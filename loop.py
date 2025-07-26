d1e3ec59649433e457fabd4836afff1232b17b82
RUSHAID-MUHAMMED-AM <rushaidmuhammedam@gmail.com>
2025-07-26 15:01:18 +0530

    sixth

--- loop.py
+++ loop.py
@@ -1,6 +1,14 @@
-#Print the even numbers from 2 to 20 using a for loop.
+# #1Print the even numbers from 2 to 20 using a for loop.
 
-for i in range(2,21):
-    if i%2==0:
-        print(i)
+# for i in range(2,21):
+#     if i%2==0:
+#         print(i)
+
+
+#2Print the sum of all numbers from 1 to 50 using a for loop
+
+k=0;
+for i in range(1,51):
+    k+=i;
+print(k);
     
