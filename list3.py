ad018d6cf14ea4405b5fd9b76aedcd682290e3ef
RUSHAID-MUHAMMED-AM <rushaidmuhammedam@gmail.com>
2025-07-26 10:18:38 +0530

    fourth

---
+++ list3.py
@@ -0,0 +1,16 @@
+'''
+Write a Python program that:
+
+Creates a list of 5 favorite fruits (any fruits you like).
+
+Uses a for loop to print:
+
+The fruit name
+
+A message like: "I like [fruit]" 
+'''
+
+
+fruits=["apple","banana","orange","grape","kiwi"];
+for fruit in fruits:
+    print("I like",fruit);
