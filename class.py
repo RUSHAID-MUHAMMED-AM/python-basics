654c0b8edcab91d00386f5f9727a766b65e7a8ca
RUSHAID-MUHAMMED-AM <rushaidmuhammedam@gmail.com>
2025-07-30 10:11:36 +0530

    eleventh

---
+++ class.py
@@ -0,0 +1,12 @@
+class Student():
+     def __init__(self,name,place):
+          self.name=name
+          self.place=place
+
+student_1=Student("Rushaid","Manjei")
+student_2=Student("Fasil","Mannarkad")
+
+print(student_1.name)
+print(student_1.place)
+print(student_2.name)
+print(student_2.place)
