6762c95294c1b815b7ef0064a63f046807030969
RUSHAID-MUHAMMED-AM <rushaidmuhammedam@gmail.com>
2025-08-04 16:06:30 +0530

    fifteenth

---
+++ inheritance2.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    def display_info(self):
+        print(f"Title: {self.title} , Author: {self.author}")
+       
+class BorrowedBook(Book):
+    def __init__(self,title,author,borrower):
+        super().__init__(title,author)
+        self.borrower=borrower
+
+    def display_info(self):
+       super().display_info()
+       print(f"Borrowed by:{self.borrower}")
+    
+
+book=BorrowedBook("Main calf","Suhail","Rushaid")
+book.display_info()
